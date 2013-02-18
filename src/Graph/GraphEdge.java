03489d36791002f6d0b530df20d4eb54f8275204
LastFlyingPig <serafael@mail.ru>
2013-02-18 07:14:24 +0400

    Добавлени документация, исправлены ошибки

--- src/Graph/GraphEdge.java
+++ src/Graph/GraphEdge.java
@@ -1,11 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Graph;
 
 /**
- *
+ * Ксласс для представления ребра графа
+ * Представляет собой уникальный ID ребра, ID вершины из который идёт ребро, ID вершины в которую идёт ребро и направление ребра 
  * @author FlyPig
  */
 public class GraphEdge {
@@ -15,7 +12,26 @@ public class GraphEdge {
     private int fromID;
     private int toID;
     private Direction direct;    
-                
+      
+    /**
+     * Конструктор для создания нового ребра графа без связи с вершинами
+     * @param ID - Уникалиный ID ребра
+     */
+    public GraphEdge(int ID)
+    {
+        this.edgeID=ID;
+        this.fromID=-1;
+        this.toID=-1;
+        this.direct=null;
+    }    
+    
+    /**
+     * Конструктор для создания нового ребра графа
+     * @param ID - Уникалиный ID ребра
+     * @param from - ID вершины из которой выходит ребро
+     * @param to - ID вершины в которую попадает ребро
+     * @param dir - Направление ребра
+     */
     public GraphEdge(int ID, int from, int to, Direction dir)
     {
         this.edgeID=ID;
@@ -24,26 +40,51 @@ public class GraphEdge {
         this.direct=dir;
     }   
     
+    /**
+     * Функция для получения ID данного ребра
+     * @return Возвращает ID данного ребра
+     */
     public int getID()
     {
-        return this.edgeID;
+        int ID=this.edgeID;
+        return ID;
     }
     
+    /**
+     * Функция для получения направления данного ребра
+     * @return Возвращает направление данного ребра
+     */
     public Direction getDirection()
     {
-        return this.direct;
+        Direction direct=this.direct;
+        return direct;
     }
     
+    /**
+     * Функция для получения ID вершины из которой выходит ребро
+     * @return Возвращает ID вершины из которой выходит ребро
+     */
     public int getFromID()
     {
-        return this.fromID;
+        int ID=this.fromID;
+        return ID;
     }
     
+    /**
+     * Функция для получения ID вершины в которую попадает ребро
+     * @return Возвращает ID вершины в которую попадает ребро
+     */
     public int getToID()
     {
-        return this.toID;
+        int ID=this.toID;
+        return ID;
     }
     
+     /**
+     * Функция для сравнения двух элементов класса GraphEdge на равенство
+     * @param edge - Ребро, которое требуется сравнить с данным
+     * @return Возвращает true, если ребра равны, false - ребра не равны
+     */
     public boolean equals(GraphEdge edge)
     {
         if(this.edgeID==edge.getID() && this.fromID==edge.getFromID() && this.toID==edge.toID && this.direct==edge.getDirection())
@@ -55,4 +96,31 @@ public class GraphEdge {
             return false;
         }
     }
+    
+    /**
+     * Функция для задания нового ID вершины из которой выходит ребро
+     * @param newFromID - Новый ID вершины из которой выходит ребро
+     */
+    public void setFrom(int newFromID)
+    {
+        this.fromID=newFromID;
+    }
+    
+    /**
+     * Функция для задания нового ID вершины в которую попадает ребро
+     * @param newToID - Новый ID вершины в которую попадает ребро
+     */
+    public void setTo(int newToID)
+    {
+        this.toID=newToID;
+    }
+    
+    /**
+     * Функция для задания нового направления ребра
+     * @param newDirection - Новое направление ребра
+     */
+     public void setDirection(Direction newDirection)
+     {
+         this.direct=newDirection;
+     }
 }
